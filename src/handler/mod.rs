7bad56d5e69ff535bc312378623c06806a9d806c
tktcorporation <tktcorporation.go@gmail.com>
2021-06-13 05:17:01 +0900

    :sparkles: reply while joinning

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,8 +1,10 @@
 use serenity::{
     async_trait,
     client::{Context, EventHandler},
-    model::gateway::Ready,
+    model::{channel::Message, gateway::Ready},
 };
+use songbird::input::Input;
+use std::sync::Arc;
 
 pub struct Handler;
 
@@ -11,4 +13,49 @@ impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
+
+    async fn message(&self, ctx: Context, msg: Message) {
+        // botに反応しないようにする
+        if msg.author.bot {
+            return;
+        }
+
+        // voice channel にいない場合は動かさない
+        if get_handler_when_in_voice_channel(&ctx, &msg)
+            .await
+            .is_none()
+        {
+            return;
+        }
+
+        // サーバーのID
+        eprintln!("guild_id = {:?}", msg.guild_id);
+        // チャンネル名
+        let channel_name = msg.channel_id.name(&ctx.cache).await;
+        eprintln!("channel_name = {:?}", channel_name);
+        // メッセージの送信
+        let content = msg.content;
+        if let Err(why) = msg.channel_id.say(&ctx.http, content).await {
+            println!("Error sending message: {:?}", why);
+        }
+    }
+}
+
+async fn get_handler_when_in_voice_channel(
+    ctx: &Context,
+    msg: &Message,
+) -> Option<Arc<serenity::prelude::Mutex<songbird::Call>>> {
+    let manager = songbird::get(&ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+    return manager.get(msg.guild(&ctx.cache).await.unwrap().id);
+}
+
+async fn speak(handler_lock: &Arc<serenity::prelude::Mutex<songbird::Call>>, input: Input) {
+    let mut handler = handler_lock.lock().await;
+    // if let Some(handler_lock) = manager.get(guild_id) {
+    //     let mut handler = handler_lock.lock().await;
+
+    handler.enqueue_source(input);
 }
