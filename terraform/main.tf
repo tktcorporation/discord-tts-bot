ad5d79614dfc0eabd55d330ddb64a7846db9fb3a
tktcorporation <tktcorporation.go@gmail.com>
2021-06-18 14:29:01 +0900

    wip

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+variable "gcp_project" {
+  default = "discord-speech-bot"
+}
+variable "billing_id" {}
+
+provider "google" {
+  project = var.gcp_project
+}
+
+resource "google_project" "gcp_project" {
+  name                = var.gcp_project
+  project_id          = var.gcp_project
+  billing_account     = var.billing_id
+  auto_create_network = false
+}
+
+resource "google_project_service" "tts" {
+  project = google_project.gcp_project.project_id
+  service = "texttospeech.googleapis.com"
+}
