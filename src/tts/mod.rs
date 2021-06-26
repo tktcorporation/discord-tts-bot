4d686a360dcd61cbdaa5aa1a2cdc0e5bb3baef06
tktcorporation <tktcorporation.go@gmail.com>
2021-06-26 11:26:36 +0900

    :sparkles: success to generate mp3

--- src/tts/mod.rs
+++ src/tts/mod.rs
@@ -1,4 +1,5 @@
 use polly::model::{Engine, Voice};
+use std::env;
 use std::error::Error;
 
 async fn describe() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
@@ -41,6 +42,91 @@ async fn describe() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     Ok(())
 }
 
+use std::fs;
+use std::process;
+
+use polly::model::{OutputFormat, VoiceId};
+use polly::{Client, Config, Region};
+
+use aws_types::region::{EnvironmentProvider, ProvideRegion};
+
+use structopt::StructOpt;
+use tokio::io::AsyncWriteExt;
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::fmt::SubscriberBuilder;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// The region
+    #[structopt(short, long)]
+    region: Option<String>,
+
+    /// The file containing the text to synthesize
+    #[structopt(short, long)]
+    filename: String,
+
+    /// Whether to show additional output
+    #[structopt(short, long)]
+    verbose: bool,
+}
+
+async fn generate_sound(filename: &str, verbose: bool) {
+    let region = EnvironmentProvider::new().region().unwrap_or_else(|| {
+        Region::new(env::var("AWS_REGION").expect("Expected a region string in the environment"))
+    });
+
+    if verbose {
+        println!("polly client version: {}\n", polly::PKG_VERSION);
+        println!("Region:   {:?}", &region);
+        println!("Filename: {}", filename);
+
+        SubscriberBuilder::default()
+            .with_env_filter("info")
+            .with_span_events(FmtSpan::CLOSE)
+            .init();
+    }
+
+    let config = Config::builder().region(region).build();
+
+    let client = Client::from_conf(config);
+
+    let content = filename;
+
+    let resp = match client
+        .synthesize_speech()
+        .output_format(OutputFormat::Mp3)
+        .text(content)
+        .voice_id(VoiceId::Joanna)
+        .send()
+        .await
+    {
+        Ok(output) => output,
+        Err(e) => {
+            println!("Got an error synthesizing speech:");
+            println!("{}", e);
+            process::exit(1);
+        }
+    };
+
+    // Get MP3 data from response and save it
+    let mut blob = resp
+        .audio_stream
+        .collect()
+        .await
+        .expect("failed to read data");
+
+    let parts: Vec<&str> = filename.split('.').collect();
+    let out_file = format!("{}{}", String::from(parts[0]), ".mp3");
+
+    let mut file = tokio::fs::File::create(out_file)
+        .await
+        .expect("failed to create file");
+
+    file.write_all_buf(&mut blob)
+        .await
+        .expect("failed to write to file");
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -59,6 +145,12 @@ mod tests {
     async fn test_describe() {
         let result = describe().await;
         result.unwrap();
+        assert_eq!(true, true);
+    }
+
+    #[tokio::test]
+    async fn test_generate_sound() {
+        let result = generate_sound("sample", true).await;
         assert_eq!(true, true);
     }
 }
