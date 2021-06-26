75da35f5bbd681b6f2ed657121a721a0e5e37396
tktcorporation <tktcorporation.go@gmail.com>
2021-06-26 11:58:19 +0900

    :sparkles: success to play japanese

--- src/tts/mod.rs
+++ src/tts/mod.rs
@@ -2,47 +2,6 @@ use polly::model::{Engine, Voice};
 use std::env;
 use std::error::Error;
 
-async fn describe() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-    let client = polly::Client::from_env();
-    let mut tok = None;
-    let mut voices: Vec<Voice> = vec![];
-    // Below is an an example of how pagination can be implemented manually.
-    loop {
-        let mut req = client.describe_voices();
-        if let Some(tok) = tok {
-            req = req.next_token(tok);
-        }
-        let resp = req.send().await?;
-        for voice in resp.voices.unwrap_or_default() {
-            println!(
-                "I can speak as: {} in {:?}",
-                voice.name.as_ref().unwrap(),
-                voice.language_name.as_ref().unwrap()
-            );
-            voices.push(voice);
-        }
-        tok = match resp.next_token {
-            Some(next) => Some(next),
-            None => break,
-        };
-    }
-    let neural_voices = voices
-        .iter()
-        .filter(|voice| {
-            voice
-                .supported_engines
-                .as_deref()
-                .unwrap_or_default()
-                .contains(&Engine::Neural)
-        })
-        .map(|voice| voice.id.as_ref().unwrap())
-        .collect::<Vec<_>>();
-
-    println!("Voices supporting a neural engine: {:?}", neural_voices);
-    Ok(())
-}
-
-use std::fs;
 use std::process;
 
 use polly::model::{OutputFormat, VoiceId};
@@ -70,7 +29,7 @@ struct Opt {
     verbose: bool,
 }
 
-async fn generate_sound(filename: &str, verbose: bool) {
+async fn generate_sound(content: String, voice_id: VoiceId, filename: &str, verbose: bool) {
     let region = EnvironmentProvider::new().region().unwrap_or_else(|| {
         Region::new(env::var("AWS_REGION").expect("Expected a region string in the environment"))
     });
@@ -90,13 +49,11 @@ async fn generate_sound(filename: &str, verbose: bool) {
 
     let client = Client::from_conf(config);
 
-    let content = filename;
-
     let resp = match client
         .synthesize_speech()
         .output_format(OutputFormat::Mp3)
         .text(content)
-        .voice_id(VoiceId::Joanna)
+        .voice_id(voice_id)
         .send()
         .await
     {
@@ -142,15 +99,14 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_describe() {
-        let result = describe().await;
-        result.unwrap();
-        assert_eq!(true, true);
-    }
-
-    #[tokio::test]
     async fn test_generate_sound() {
-        let result = generate_sound("sample", true).await;
+        generate_sound(
+            String::from("おはようございます"),
+            VoiceId::Mizuki,
+            "sample",
+            true,
+        )
+        .await;
         assert_eq!(true, true);
     }
 }
