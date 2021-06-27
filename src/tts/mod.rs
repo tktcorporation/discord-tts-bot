754a832ec5e3a404dc9b598f09a1445076c655e1
tktcorporation <tktcorporation.go@gmail.com>
2021-06-28 07:59:54 +0900

    :recycle: remove unused dependencies

--- src/tts/mod.rs
+++ src/tts/mod.rs
@@ -95,16 +95,6 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn https_access_requwest() {
-        let url = String::from("https://www.google.co.jp/");
-        let client = reqwest::Client::new();
-        let res = client.get(&url).send().await.unwrap();
-        assert_eq!(&res.status().as_str()[..], "200");
-        let body = &res.text().await.unwrap();
-        assert!(&body.len() > &(100 as usize));
-    }
-
-    #[tokio::test]
     async fn test_generate_sound() {
         let root = option_env!("CARGO_MANIFEST_DIR").unwrap();
         let path = Path::new(root);
