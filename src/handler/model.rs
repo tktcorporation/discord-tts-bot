05af5d9002fa7cc6ba32119d9562f708ec78046e
tktcorporation <tktcorporation.go@gmail.com>
2021-08-30 01:21:38 +0900

    :sparkles: Exit when everyone except the bot leaves

--- src/handler/model.rs
+++ src/handler/model.rs
@@ -14,12 +14,14 @@ pub struct Speaker {
     /// true -> new speaker
     /// false -> leaved speaker
     pub is_new: bool,
+    pub member_count: usize,
 }
 
 impl CurrentVoiceState {
     pub fn new(state: voice::VoiceState) -> Self {
         CurrentVoiceState { state }
     }
+
     /// [`previous_voice_state`] が空で、現在の [`voice::VoiceState`] も Seaker として動いている場合に [`NewSpeaker`] を返す
     pub async fn new_speaker(
         self,
@@ -36,6 +38,14 @@ impl CurrentVoiceState {
         if current_user_id == user.id {
             return Err("This is me(bot). My entering is ignored.");
         }
+
+        let channel_id = if self.state.channel_id.is_some() {
+            self.state.channel_id.unwrap()
+        } else {
+            previous_voice_state.clone().unwrap().channel_id.unwrap()
+        };
+        let members = members(ctx, &guild_id, &channel_id).await.unwrap().len();
+
         match previous_voice_state {
             // 他サーバーに反応しないように
             Some(_) => {
@@ -44,6 +54,7 @@ impl CurrentVoiceState {
                         guild_id,
                         user,
                         is_new: false,
+                        member_count: members,
                     })
                 } else {
                     Err("This is not a new speaker. The previous is not None.")
@@ -53,7 +64,20 @@ impl CurrentVoiceState {
                 guild_id,
                 user,
                 is_new: true,
+                member_count: members,
             }),
         }
+    }
+}
+
+async fn members(
+    ctx: &Context,
+    guild_id: &id::GuildId,
+    channel_id: &id::ChannelId,
+) -> std::result::Result<std::vec::Vec<serenity::model::guild::Member>, String> {
+    let channels = guild_id.channels(&ctx.http.as_ref()).await.unwrap();
+    match channels.get(channel_id) {
+        Some(guild_channel) => Ok(guild_channel.members(&ctx.cache).await.unwrap()),
+        _ => Err("can't get a channel id".to_string()),
     }
 }
